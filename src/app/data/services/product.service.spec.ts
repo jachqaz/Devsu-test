e0e3f79c5f6476f107ce4c7d6e12a428b1ca2e3b
Johan Castaño <jach_1997@hotmail.com>
2024-10-13 09:53:42 -0500

    Jest for testing is added to the project
    Signed-off-by: Johan Castaño <jach_1997@hotmail.com>

--- src/app/data/services/product.service.spec.ts
+++ src/app/data/services/product.service.spec.ts
@@ -1,16 +1,24 @@
 import {TestBed} from '@angular/core/testing';
 
 import {ProductService} from './product.service';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 
 describe('ProductService', () => {
-  let service: ProductService;
+  let service: ProductService, httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ProductService]
+    });
     service = TestBed.inject(ProductService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+  afterEach(() => {
+    httpMock.verify();
   });
 });
--- src/app/presentation/components/product/product.component.spec.ts
+++ src/app/presentation/components/product/product.component.spec.ts
@@ -1,6 +1,9 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {ProductComponent} from './product.component';
+import {provideRouter} from '@angular/router';
+import {routes} from '../../app.routes';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 
 describe('ProductComponent', () => {
   let component: ProductComponent;
@@ -8,7 +11,8 @@ describe('ProductComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ProductComponent]
+      imports: [ProductComponent, HttpClientTestingModule],
+      providers: [provideRouter(routes)]
     })
       .compileComponents();
 
